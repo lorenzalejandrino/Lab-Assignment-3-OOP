4778f9476a8bb18578dbd56f32693d6268600285
lorenzalejandrino <158862246+lorenzalejandrino@users.noreply.github.com>
2024-03-02 18:26:44 +0800

    Delete ShopeePay.java

--- ShopeePay.java
+++
@@ -1,8 +0,0 @@
-package payment;
-
-public class ShopeePay implements PaymentMode {
-    @Override
-    public double calculateDiscount(double totalAmount) {
-        return 0.10 * totalAmount;
-    }
-}
